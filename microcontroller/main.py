2c1f403fd3be999a93fb7beb81765f1b4173575c
William Lam <williamntlam@gmail.com>
2025-05-25 18:45:53 -0400

    feat: created main.py for microcontroller usuage (m5go)

---
+++ microcontroller/main.py
@@ -0,0 +1,10 @@
+from m5stack import *
+from m5ui import *
+from uiflow import *
+
+
+setScreenColor(0x222222)
+
+setScreenColor(0x222222)  # Dark background
+lcd.print("hello", 10, 10, 0xFFFFFF)  # Text, x, y, color (white)
+
